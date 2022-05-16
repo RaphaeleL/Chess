0405ea9a3ab6cbe828e3eaa9a25eea81ae65bcf9
raphaele <raphaele.salvatore@outlook.de>
2022-05-16 10:01:53 +0200

    Bishop Movements

--- src/Pieces/Rook.java
+++ src/Pieces/Rook.java
@@ -2,11 +2,8 @@ package Pieces;
 
 import Board.Board;
 import Utils.Movement;
-
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class Rook extends Piece {
 
