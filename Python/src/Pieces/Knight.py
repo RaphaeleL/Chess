ef51ad625037b0c976f451e5bbe26bef34c6247b
raphaele <raphaele.salvatore@outlook.de>
2022-05-17 10:57:49 +0200

    [Python] Knight moves

--- Python/src/Pieces/Knight.py
+++ Python/src/Pieces/Knight.py
@@ -1,4 +1,5 @@
-from Pieces.Piece import Piece
+from Pieces.Piece import Piece, Empty
+from Utils.Movement import Movement
 
 class Knight(Piece):
     def __init__(self, side, value, representation):
@@ -10,4 +11,13 @@ class Knight(Piece):
         return self.representation
 
     def checkMove(self, board, move, side):
-        return True
+        movementUtil = Movement()
+        allowedMovements = []
+        allowedKeys = [[2, 1], [2, -1], [-2, 1], [-2, -1], [1, -2], [1, 2], [-1, -2], [-1, 2]]
+        for allowedKey in allowedKeys:
+            x = move[0] - allowedKey[0]
+            y = move[1] - allowedKey[1]
+            if movementUtil.inBounds(x, y):
+                if movementUtil.isFree(board, x, y, side, board.getFigure(move[0], move[1])):
+                    allowedMovements.append([x, y])
+        return movementUtil.isAllowed(allowedMovements, move)
