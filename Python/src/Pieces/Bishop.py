33d2b95b6a0b9ac6405409ec023a730302d1a6cc
raphaele <raphaele.salvatore@outlook.de>
2022-05-17 11:19:12 +0200

    [Python] bishop movements

--- Python/src/Pieces/Bishop.py
+++ Python/src/Pieces/Bishop.py
@@ -1,4 +1,5 @@
-from Pieces.Piece import Piece
+from Pieces.Piece import Piece, Empty
+from Utils.Movement import Movement
 
 class Bishop(Piece):
     def __init__(self, side, value, representation):
@@ -9,5 +10,57 @@ class Bishop(Piece):
     def __str__(self): 
         return self.representation
 
+    def getAllBishopMovesA(self, move): 
+        movement = Movement()
+        allMovements = []
+        while (movement.inBounds(move[0], move[1])):
+            move[0] -= 1 
+            if not [move[0], move[1]] in allMovements: 
+                allMovements.append([move[0], move[1]])
+        return allMovements
+
+    def getAllBishopMovesB(self, move): 
+        movement = Movement()
+        allMovements = []
+        while (movement.inBounds(move[0], move[1])):
+            move[0] += 1 
+            if not [move[0], move[1]] in allMovements: 
+                allMovements.append([move[0], move[1]])
+        return allMovements
+
+    def getAllBishopMovesC(self, move): 
+        movement = Movement()
+        allMovements = []
+        while (movement.inBounds(move[0], move[1])):
+            move[1] -= 1 
+            if not [move[0], move[1]] in allMovements: 
+                allMovements.append([move[0], move[1]])
+        return allMovements
+
+    def getAllBishopMovesD(self, move): 
+        movement = Movement()
+        allMovements = []
+        while (movement.inBounds(move[0], move[1])):
+            move[1] += 1 
+            if not [move[0], move[1]] in allMovements: 
+                allMovements.append([move[0], move[1]])
+        return allMovements
+
     def checkMove(self, board, move, side):
-        return True
+        movementUtil = Movement()
+        allowedMovements = []
+        allMovements = [self.getAllBishopMovesA(move), self.getAllBishopMovesB(move), 
+            self.getAllBishopMovesC(move), self.getAllBishopMovesD(move)]
+        for ints in allMovements: 
+            for movement in ints: 
+                x = movement[0]
+                y = movement[1]
+                if movementUtil.inBounds(x, y): 
+                    if type(board.getFigure(x, y)) is Empty("o").__class__:
+                        if movementUtil.isFree(board, x, y, side, board.getFigure(move[0], move[1])):
+                            allowedMovements.append([x, y])
+                    else: 
+                        if board.getFigure(x, y).side != board.getFigure(move[0], move[1]).side: 
+                            allowedMovements.append([x, y])
+                        break
+        return movementUtil.isAllowed(allowedMovements, move)
