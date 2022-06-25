0b8cd0f8567583a16d9b7ea8a63fec6361dcc776
raphaele <raphaele.salvatore@outlook.de>
2022-06-25 10:34:04 +0200

    [Ruby] interact movement

--- Ruby/src/Utils/Movement.rb
+++ Ruby/src/Utils/Movement.rb
@@ -14,3 +14,18 @@ def dearScreen()
     puts "================================================================="
     puts "================================================================="
 end
+
+def interactMove(movement, board) 
+    fromPiece = board[movement[0]][movement[1]] 
+    board[movement[0]][movement[1]] = 0
+    board[movement[2]][movement[3]] = fromPiece
+end 
+
+def makeMove(board, side) 
+    movement = getInput(side)
+    if check(movement, board, side)
+        interactMove(movement, board)
+        return true
+    end
+    return false
+end 
