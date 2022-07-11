1eddc3841e9287e17b40095cd85284ce15471853
raphaele <raphaele.salvatore@outlook.de>
2022-07-11 13:58:22 +0200

    [Ruby] handle input

--- Ruby/src/Utils/Movement.rb
+++ Ruby/src/Utils/Movement.rb
@@ -29,3 +29,30 @@ def makeMove(board, side)
     end
     return false
 end 
+
+def prepare(userInputSplitted)
+    alphabet = "abcdefgh"
+    fromX = alphabet.index(userInputSplitted[0])
+    toX = alphabet.index(userInputSplitted[2])
+    if is_number?(userInputSplitted[1])
+        fromY = userInputSplitted[1].to_i - 1
+    end
+    if is_number?(userInputSplitted[3])
+        toY = userInputSplitted[3].to_i - 1
+    end
+    return fromY, fromX, toY, toX
+end
+
+def is_number? string
+    true if Float(string) rescue false
+end
+
+def getInput(side) 
+    if side 
+        print "WHITE [From,To]> "
+    else 
+        print "BLACK [From,To]> "
+    end
+    userInputSplitted = gets.chomp.scan /\w/
+    return prepare(userInputSplitted)
+end 
